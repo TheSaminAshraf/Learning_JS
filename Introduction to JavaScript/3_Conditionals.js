6b7bb5888bc537e2774be9194a9b581c11a05306
SaminAshraf02 <sashraf24@myseneca.ca>
2025-04-25 06:39:40 -0400

    Eslint

---
+++ Introduction to JavaScript/3_Conditionals.js
@@ -0,0 +1,28 @@
+/*
+
+### What are Conditional Statements? ###
+
+If-else decisions can be modeled in code by creating conditionals statements. A conditional
+statement checks a specific condition(s) and performs a task based on the condition(s).
+
+We will be covering the following concepts:
+
+1. if, else if, and else statements
+2. comparison operators
+3. logical operators
+4. truthy vs falsy values
+5. ternary operators
+6. switch statement
+
+### If Statement ###
+
+We often perform a task based on a condition. In programming, we can also perform a task based on
+a condition using an if statement. The if statement is composed of:
+
+1. The if keyword followed by a set of parentheses () which is followed by a code block, or block
+statement, indicated by a set of curly braces {}.
+2. Inside the parentheses (), a condition is provided that evaluates to true or false.
+3. If the condition evaluates to true, the code inside the curly braces {} runs, or executes.
+
+
+*/
